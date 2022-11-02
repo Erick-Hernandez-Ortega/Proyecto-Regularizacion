0d8133eaf5f3d9e1b522b9fb65f2307dc4c82223
ibaggarcia <105960135+ibaggarcia@users.noreply.github.com>
2022-11-02 00:45:21 -0600

    Update user.php

--- src/php/user.php
+++ src/php/user.php
@@ -1,6 +1,8 @@
 <?php
 include("db.php");
 
+$host = $_SERVER['HTTP_HOST'];
+
 $password = $_POST['password'];
 $email = $_POST['email'];
 
@@ -11,17 +13,18 @@ if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
     if ($row == '') {
         $_SESSION['mensaje'] = "El correo y/o la contraseña son incorrectos...";
         $_SESSION['color'] = 'danger';
-        header("location: login.php");
+        header("location: http://$host/Proyecto-Regularizacion/login.php");
     } else {
         $id = $row['Id_usuario'];
         $consulta = "SELECT Nombre_de_usuario FROM usuarios_regulacion WHERE Id_usuario = '$id'";
         $query_result = mysqli_query($conn, $consulta);
         $usuario = mysqli_fetch_array($query_result);
         $_SESSION['Usuario'] = $usuario['Nombre_de_usuario'];
-        header("location: index.php");
+        header("location: http://$host/Proyecto-Regularizacion/index.php");
     }
 } else {
     $_SESSION['mensaje'] = "Ingresa un correo válido...";
     $_SESSION['color'] = 'danger';
-    header("location: login.php");
+    
+    header("location: http://$host/Proyecto-Regularizacion/login.php");
 }
