39f89e72101366b61860f5352e276122b6d438c8
Erick <erickramon47@live.com.mx>
2022-11-04 21:53:26 -0600

    Se agrega un icono al modal de busqueda

--- admin.php
+++ admin.php
@@ -583,6 +583,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="exampleModalLabel">Búsqueda Avanzada</h1>
+                    <span class="material-icons mx-1">&#xe8b6;</span>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
