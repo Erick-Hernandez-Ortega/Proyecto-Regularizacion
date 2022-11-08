91ffd937cd4403d70d7fd7cac68615083b7d79e1
PCLFRANKO <pacoygus@gmail.com>
2022-11-08 17:42:26 -0600

    Prueba

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
 </head>
 
 <body>
-
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, accusamus molestiae suscipit deserunt fugit unde itaque animi! Odio quisquam veritatis, repellendus commodi quibusdam repellat, ad magnam facilis eaque nihil vel.</p>
     <header class="p-3 text-bg-light">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
