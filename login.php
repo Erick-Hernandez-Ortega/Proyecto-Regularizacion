d8e0a2e68e4365c3726051fb79e16ad2c539a541
Erick <erickramon47@live.com.mx>
2022-11-22 00:11:16 -0600

    Se cambia estilo login

--- login.php
+++ login.php
@@ -20,11 +20,11 @@
 </head>
 
 <body>
-    <header class="p-3 text-bg-light">
+    <header class="p-3">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none">
-                    <img alt="" src="src/img/Logo 2.png" class="bi" width="63" height="65" role="img">
+                    <img alt="" src="src/img/Imagen_2_HD.png" class="bi" width="63" height="65" role="img">
                 </a>
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 </ul>
