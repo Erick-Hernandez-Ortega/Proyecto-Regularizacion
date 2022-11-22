5a1a87511580dae9a38620a24bb07ac00b249c08
josejaimrgcr <jaimecastaneda@gmail.com>
2022-11-22 16:50:16 -0600

    Cambio adicionales

--- archivado.php
+++ archivado.php
@@ -18,11 +18,11 @@ if(!isset($_SESSION['reloadarchivado'])){header('Refresh: 0'); $_SESSION['reload
 </head>
 
 <body>
-    <header class="p-3 text-bg-light">
+    <header class="p-3">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none">
-                    <img alt="" src="src/img/Logo 2.png" class="bi" width="63" height="65" role="img">
+                    <img alt="" src="src/img/Imagen_2_HD.png" class="bi" width="63" height="65" role="img">
                 </a>
 
                 <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 w-25" role="search">
@@ -37,36 +37,48 @@ if(!isset($_SESSION['reloadarchivado'])){header('Refresh: 0'); $_SESSION['reload
 
                 <div class="d-flex">
                     <span class="material-icons icono">&#xe853;</span>
-                    <h6 class="mt-2"><?php if (isset($_SESSION['Usuario'])) {
-                                            echo $_SESSION['Usuario'];
-                                        } else {
-                                            $_SESSION['mensaje'] = "Necesitas iniciar sesión...";
-                                            $_SESSION['color'] = 'danger';
-                                            $_SESSION['destroy'] = true;
-                                            $host = $_SERVER['HTTP_HOST'];
-                                            header("location: http://$host/Proyecto-Regularizacion/login.php");
-                                        } ?></h6>
+                    <h6 class="mt-2 usuario"><?php if (isset($_SESSION['Usuario'])) {
+                                                    echo $_SESSION['Usuario'];
+                                                } else {
+                                                    $_SESSION['mensaje'] = "Necesitas iniciar sesión...";
+                                                    $_SESSION['color'] = 'danger';
+                                                    $_SESSION['destroy'] = true;
+                                                    $host = $_SERVER['HTTP_HOST'];
+                                                    header("location: http://$host/Proyecto-Regularizacion/login.php");
+                                                } ?></h6>
                 </div>
 
-                <nav class="navbar bg-light border-2" aria-label="Light offcanvas navbar">
+                <nav class="navbar border-2" aria-label="Light offcanvas navbar">
                     <div class="container-fluid">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarLight" aria-controls="offcanvasNavbarLight">
-                            <span class="navbar-toggler-icon"></span>
+                        <button class="navbar-toggler" type="button" style="border: #f9f9f9 1px solid;" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarLight" aria-controls="offcanvasNavbarLight">
+                            <span class="material-icons usuario" style="font-size: 35px;">&#xe5d2;</span>
+
                         </button>
-                        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarLight" aria-labelledby="offcanvasNavbarLightLabel">
+                        <div class="offcanvas offcanvas-end c-tinto" tabindex="-1" id="offcanvasNavbarLight" aria-labelledby="offcanvasNavbarLightLabel">
                             <div class="offcanvas-header">
-                                <h5 class="offcanvas-title" id="offcanvasNavbarLightLabel">Mi cuenta</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                <h5 class="offcanvas-title usuario" id="offcanvasNavbarLightLabel">Mi cuenta</h5>
+                                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                             </div>
                             <div class="offcanvas-body">
                                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                    <li class="nav-item">
-                                        <a class="nav-link active" aria-current="page" href="#">Configuración</a>
+                                    <li class="nav-item mb-3">
+                                        <a href="registro.php" class="nav-link btn btn-success text-light" id="boton1">Registrar una cuenta</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link text-danger" href="src/php/logout.php">Cerrar Sesión</a>
+                                        <a class="nav-link btn btn-dark text-light" href="src/php/logout.php">Cerrar Sesión</a>
                                     </li>
                                 </ul>
+                            </div>
+                            <div class="row align-items-end p-4">
+                                <div class="col">
+                                    <img alt="" src="src/img/Imagen_2_HD.png" class="bi" width="70" height="65" role="img">
+                                </div>
+                                <div class="col">
+                                    <img alt="" src="src/img/Imagen_1_HD.png" class="bi" width="60" height="65" role="img">
+                                </div>
+                                <div class="col">
+                                    Hola
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -94,10 +106,11 @@ if(!isset($_SESSION['reloadarchivado'])){header('Refresh: 0'); $_SESSION['reload
     <!-- <div class="agregar-folio mt-3">
         <a href="admin.php" class="btn btn-primary">Página principal</a>
     </div> -->
-
+    <div class="text-center mt-2 bg-dark bg-gradient text-white" id="tituloArchivado">
+        <p class="h1" ><strong>Archivados</strong></p>
+    </div>
     <div class="divTabla mt-3">
         <table class="table table-hover text-center caption-top" id="tablaCentral">
-            <caption style="color: white; font-weight: bold;">Archivados</caption>
             <thead>
                 <tr class="table-dark">
                     <th scope="col">FOLIO</th>
@@ -983,53 +996,36 @@ if(!isset($_SESSION['reloadarchivado'])){header('Refresh: 0'); $_SESSION['reload
         </div>
     </div>
 
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 
-    <!-- FOOTER -->
-    <footer class="w-100 py-4 flex-shrink-0 text-bg-light">
+
+   <!-- FOOTER -->
+   <footer class="w-100 py-4 flex-shrink-0 mt-4">
         <div class="container py-3">
             <div class="row gy-4 gx-5">
                 <div class="col-lg-4 col-md-6">
-                    <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none">
-                        <img alt="" src="src/img/Logo 1.png" class="bi" width="66" height="80" role="img">
+                    <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none">
+                        <img alt="" src="src/img/Imagen_1_HD.png" class="bi" width="66" height="80" role="img">
                     </a>
-                    <p class="text-muted">Ayuntamiento de Tonalá</p>
-                    <p class="text-muted">&copy; Copyrights. Todos los derechos reservados.</p>
+                    <p class="usuario">Ayuntamiento de Tonalá</p>
+                    <p class="usuario">&copy; Copyrights. Todos los derechos a los alumnos del CUT 🥵.</p>
                 </div>
                 <div class="col-lg-2 col-md-6">
-                    <h5 class="text-black mb-3">Direccion</h5>
-                    <p class="small text-muted">Calle Pino Suarez NO.32, Tonalá Centro C.P. 45400, Tonalá, Jalisco.</p>
+                    <h5 class="usuario mb-3">Dirección</h5>
+                    <p class="small usuario">Calle Pino Suarez NO.32, Tonalá Centro C.P. 45400, Tonalá, Jalisco.</p>
                 </div>
                 <div class="col-lg-2 col-md-6">
-                    <h5 class="text-black mb-3">Redes</h5>
-                    <ul class="list-unstyled text-muted">
-                        <li>
-                            <a href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" fill="black" class="bi bi-whatsapp" viewBox="0 0 16 16">
-                                    <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
-                                </svg>
-                            </a>
-                        </li>
-                        <li class="iconos">
-                            <a href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" fill="black" class="bi bi-facebook" viewBox="0 0 16 16">
-                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-                                </svg>
-                            </a>
-                        </li>
-                        <li class="iconos">
-                            <a href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" fill="black" class="bi bi-twitter" viewBox="0 0 16 16">
-                                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-                                </svg>
-                            </a>
-                        </li>
-                    </ul>
+                    <!-- aca algo chido -->
                 </div>
                 <div class="col-lg-4 col-md-6">
-                    <h5 class="text-black mb-3">Contacto</h5>
-                    <ul class="list-unstyled text-muted">
-                        <li><a href="#" class="text-decoration-none">Tel. 33-35-86-60-00</a></li>
-                        <li><a href="https://tonala.gob.mx/portal/" target="blank" class="text-decoration-none">www.tonalá.gob.mx</a></li>
+                    <h5 class="usuario mb-3">Contacto</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="tel:+523335866000" class="link-light">Tel. 33-35-86-60-00</a></li>
+                        <li><a href="https://www.tonala.gob.mx" target="_blank" class="link-light">www.tonalá.gob.mx</a></li>
                     </ul>
                 </div>
             </div>
